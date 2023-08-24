eca12f92e4521035e69d3d16637f239dc55747b0
Nitu Dubey <nitutiwari01@gmail.com>
2023-08-24 05:25:20 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,40 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-const [username, setUsername] = useState("")
-const [password, setPassword] = useState("")
-const [error, setError] = useState("")
-
-function handleLoginBtn(){
-    if(!username || !password){
-      setError("Both username and password are required.")
+  function handleLoginBtn() {
+    if (!username || !password) {
+      setError("Both username and password are required.");
     }
-}
+  }
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        Username: <input type="text" onChange={(e)=>setUsername(e.target.value)} value={username}/><br/><br/>
-        password: <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} />
-        <p id="errorMessage" style={{color:"red"  }}>{error}</p>
-        <button onClick={handleLoginBtn}>Login</button>
+      {/* Do not remove the main div */}
+      Username:
+      <input
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+      />
+      <br />
+      <br />
+      password:
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <p id="errorMessage" style={{ color: "red" }}>
+        {error}
+      </p>
+      <button onClick={handleLoginBtn}>Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
